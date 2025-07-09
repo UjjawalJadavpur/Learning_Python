dad91ae4981a00dcbfabb45537e862c23cf3ad21
UJJAWAL JADAVPUR <ujjawalkjadavpur@gmail.com>
2025-07-09 13:53:33 +0530

    first program

---
+++ FirstProgram.py
@@ -0,0 +1,3 @@
+print("hello world")
+print("Ladakh is located at the trijunction of India, China, & Pakistan.")
+print("The DM system is a colonial legacy", "that still operates in democratic India")
