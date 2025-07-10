9678e7e547f234625cf86255cc662399679c1ba4
UJJAWAL JADAVPUR <ujjawalkjadavpur@gmail.com>
2025-07-10 07:27:34 +0530

    operator done

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,3 +1,42 @@
 print("hello world")
 print("Ladakh is located at the trijunction of India, China, & Pakistan.")
 print("The DM system is a colonial legacy", "that still operates in democratic India")
+print(23)
+print(3567)
+print("value for 56+12 is : ", 56+12)
+print("value for 56-12 is : ", 56-12)
+print("value for 56*12 is : ", 56*12)
+print("value for 56/12 is : ", 56/12)
+print("value for 56%12 is : ", 56%12)
+print(45.67)
+
+# single line comment
+"""
+this is multi line comment
+"""
+
+name = "Mango"
+quanity = 5
+price = 25.99
+_ripe = True
+code = None
+
+print(name)
+print(type(name))
+print(quanity)
+print(type(quanity))
+print(price)
+print(type(price))
+print(_ripe)
+print(type(_ripe))
+print(code)
+print(type(code))
+print(name, quanity, price, _ripe)
+
+name1 = 'apple'
+name2 = "appleMango"
+name3 = '''applePears'''
+print(name1)
+print(name2)
+print(name3)
+
---
+++ Operator.py
@@ -0,0 +1,72 @@
+# Airthmetic Operator
+
+print("Airthmetic Operator")
+
+a = 5
+b = 2
+print("a =  ",a)
+print("b =  ",b)
+print("sum      : ", a + b)
+print("diff     : ", a - b)
+print("multiply : ", a * b)
+print("divide   : ", a / b)
+print("Div_inte : ", a // b)
+print("Modulo   : ", a % b)
+print("power    : ", a ** b)
+
+# Relational/Comparison Operator
+print("Relational/Comparison Operator")
+
+p = 50
+q = 20
+
+print("p =  ",p)
+print("q =  ",q)
+print("Is p == q  : ", p == q)
+print("Is p != q  : ", p != q)
+print("Is p >= q  : ", p >= q)
+print("Is p > q   : ", p > q)
+print("Is p <= q  : ", p <= q)
+print("Is p < q   : ", p < q)
+
+
+# Assignment Operator
+print("Assignment Operator")
+
+num = 10
+print("num : ",num)
+num += 10
+print("num : ",num)
+num -+ 10
+print("num : ",num)
+num *= 10
+print("num : ",num)
+num /= 10
+print("num : ",num)
+num += 10
+num %= 10
+print("num : ",num)
+num += 10
+num **= 10
+print("num : ",num)
+
+# logical Operator - Not and or --- return boolean
+print("logical Operator - not, and, or ")
+
+print(not False)
+print(not True)
+
+
+val1 = True
+val2 = True
+val3 = False
+val4 = False
+print(" val1 : ",val1)
+print(" val1 : ",val1)
+print(" val1 : ",val3)
+print(" val1 : ",val4)
+print("val1 and val2  : ", val1 and val2)
+print("val1 and val3  : ", val1 and val3)
+print("val1 or val3   : ", val1 or val3)
+print("val1 or val4   : ", val1 or val4)
+print("OR operator    : ", (a == b) or (a > b))
