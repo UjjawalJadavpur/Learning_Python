2a10c2d1c24e806c8fd8db6d4d775b811ff35be4
UJJAWAL JADAVPUR <ujjawalkjadavpur@gmail.com>
2025-07-16 17:51:48 +0530

    practice program

---
+++ practice2.py
@@ -0,0 +1,32 @@
+
+print("Try programiz.pro")
+x = "10"
+y = 5
+print(x * y)
+print(bool(0), bool(1), bool(""), bool("hello"))
+n=2
+def check(n):
+    if n > 0:
+        print("Postive")
+    elif n < 0:
+        print("Negative")
+    else:
+        print("Zero")
+    
+for i in range(11):
+    print(i)
+    
+sum = 0
+i = 1
+while i < 6:
+    sum = sum + i
+    i = i+1
+print(sum)
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+
+
